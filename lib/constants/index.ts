d2c9e0ff423255fbcdaa90ca797c23d1800c03f2
AbdoAnss <abdoanss@proton.me>
2025-05-10 21:16:26 +0100

    Initial commit with new content

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Aseds';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A modern store built with Next.js and prisma';
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
