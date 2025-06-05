c3d8f8302d641e6e05c322f6b7facf4473dbd100
simo126 <simo82220@gmail.com>
2025-06-05 17:24:55 +0100

    Added User Profile

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,9 +1,23 @@
 import { Metadata } from "next";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
+import ProfileForm from "./profile-form";
 
 export const metadata: Metadata = {
   title: "Customer Profile",
 };
 
-export default async function ProfilePage() {
-  return <>Profile</>;
-}
+const Profile = async () => {
+  const session = await auth();
+
+  return (
+    <SessionProvider session={session}>
+      <div className="max-w-md mx-auto space-y-4">
+        <h2 className="h2-bold">Profile</h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
+  );
+};
+
+export default Profile;
